e75f620d27718027a67f44b2d66b5383ed33e07a
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-23 11:49:22 -0400

    app.js finished theoretically

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
-const routes = require("./routes/router")
+const customer = require("./routes/customer")
+const vendor = require("./routes/vendor")
 
 const app = express()
 
@@ -14,7 +15,8 @@ app.set("views", path.join(__dirname, "templates"))
 app.set("view engine", "mustache")
 app.use(express.static("public"))
 
-app.use(routes)
+app.use(customer)
+app.use(vendor)
 
 app.listen(3000, () => {
   console.log("I've got the magic in me!")
