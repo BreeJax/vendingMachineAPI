06b5eb792b984a37ec9959ac20c22dbb35d5bcd5
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-23 11:56:43 -0400

    everything is connected correctly- all is working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
+//const routes = require("./routes/router")
 const customer = require("./routes/customer")
 const vendor = require("./routes/vendor")
+const mustacheExpress = require("mustache-express")
 
 const app = express()
 
@@ -15,6 +17,7 @@ app.set("views", path.join(__dirname, "templates"))
 app.set("view engine", "mustache")
 app.use(express.static("public"))
 
+// app.use(routes)
 app.use(customer)
 app.use(vendor)
 
---
+++ routes/customer.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const models = require("../models")
+
+console.log("hello")
+
+module.exports = router
