707474f88017744fb72eaa75920809f9a24ffbfc
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-24 14:17:16 -0400

    customer posting updated so that inventory/quantity will decress

--- routes/customer.js
+++ routes/customer.js
@@ -2,6 +2,11 @@ const express = require("express")
 const customer = express.Router()
 const models = require("../models")
 
+// customer.get("/api/customer/items", (req, res) => {
+//   console.log("hello This")
+//   res.json(Items)
+// })
+
 customer.get("/api/customer/items", function(req, res) {
   models.Items
     .findAll()
@@ -13,17 +18,19 @@ customer.get("/api/customer/items", function(req, res) {
     })
 })
 
-//need to find a way of saying ()
+//need to find o
 customer.post("/api/customer/items/:itemId/purchases", (req, res) => {
   models.Items.findById(req.params.itemId).then(item => {
-    if (req.body.quantity >= 1) {
-      models.Items.update()
-    }
     let changeBack = req.body.amount - item.itemCost
     const purchase = models.Purchases.build({
       moneyInMachine: item.itemCost,
       itemId: item.id,
       purchasedAt: Date.now()
+    })
+
+    item.quantity -= 1
+    item.save().then(item => {
+      console.log("updated the inventory")
     })
 
     purchase.save().then(moneyReturnToUser => {
@@ -33,3 +40,5 @@ customer.post("/api/customer/items/:itemId/purchases", (req, res) => {
 })
 
 module.exports = customer
+
+// POST /api/customer/items/:itemId/purchases - purchase an item
