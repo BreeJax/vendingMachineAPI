45033110654a5f1921979b155c1876f42b8878bf
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-24 15:15:08 -0400

    customers purchases improved upon with an if else statement

--- routes/customer.js
+++ routes/customer.js
@@ -21,21 +21,25 @@ customer.get("/api/customer/items", function(req, res) {
 //need to find o
 customer.post("/api/customer/items/:itemId/purchases", (req, res) => {
   models.Items.findById(req.params.itemId).then(item => {
-    let changeBack = req.body.amount - item.itemCost
-    const purchase = models.Purchases.build({
-      moneyInMachine: item.itemCost,
-      itemId: item.id,
-      purchasedAt: Date.now()
-    })
+    if (item.quantity > 0) {
+      let changeBack = req.body.amount - item.itemCost
+      const purchase = models.Purchases.build({
+        moneyInMachine: item.itemCost,
+        itemId: item.id,
+        purchasedAt: Date.now()
+      })
 
-    item.quantity -= 1
-    item.save().then(item => {
-      console.log("updated the inventory")
-    })
+      item.quantity -= 1
+      item.save().then(item => {
+        console.log("updated the inventory")
+      })
 
-    purchase.save().then(moneyReturnToUser => {
-      res.json(moneyReturnToUser)
-    })
+      purchase.save().then(moneyReturnToUser => {
+        res.json(moneyReturnToUser)
+      })
+    } else {
+      res.json("sorry, no inventory")
+    }
   })
 })
 
